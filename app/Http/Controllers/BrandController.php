b4d69b1b8a6b603f9b943a3bc209233eabc4fcb6
Asaduzzaman <ajaman375@gmail.com>
2022-04-06 03:50:06 -0700

    brand inserted ok image not display

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class BrandController extends Controller
 {
@@ -21,5 +23,20 @@ class BrandController extends Controller
                 'brand_name.required' => 'Brand Name is required'
             ]
         );
+        $brand_img = $request->file('brand_img');
+        $name_gen = hexdec(uniqid());
+        $img_ext = strtolower($brand_img->getClientOriginalExtension());
+        $img_name = $name_gen.'.'.$img_ext;
+        $up_location = 'images/brand';
+        $last_img = $up_location.$img_name;
+        $brand_img->move($up_location.$img_name);
+
+        Brand::insert([
+            'brand_name' => $request->brand_name,
+            'brand_img' => $last_img,
+            'created_at' => Carbon::now()
+        ]);
+
+        return Redirect()->back()->with('success','Brand added successfully');
     }
 }
